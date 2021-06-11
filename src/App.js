3a3bc39d07b7b3b7894e022c663ed52262637117
luanpires94 <luan.94pires@gmail.com>
2021-06-11 16:08:51 -0300

    update

--- src/App.js
+++ src/App.js
@@ -19,31 +19,32 @@ const ImgsMovie = styled.img`
 `
 
 const ContainerMain = styled.div`
-background-color: #F4F4F4;
+  height: 100vh;
+  background-color: #F4F4F4;
 `
 
 const InputSearch = styled.input`
-    width: 100%;
-    height: 56px;
-    background: #FFFFFF 0% 0% no-repeat;
-    border-radius: 6px;
-    font-size: 24px;
-    padding: 10px;
+  width: 100%;
+  height: 56px;
+  background: #FFFFFF 0% 0% no-repeat;
+  border-radius: 6px;
+  font-size: 24px;
+  padding: 10px;
 `
 
 const Title = styled.h1`
-    font-size: 40px;
-    padding: 1rem 0;
+  font-size: 40px;
+  padding: 1rem 0;
 `
 
 const BoxTitleSearch = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-evenly;
-    height: 25vh;
-    background-color: #000;
-    color: #fff;
-    padding: 9%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  height: 25vh;
+  background-color: #000;
+  color: #fff;
+  padding: 9%;
 `
 const Container = styled.div`
   width: 100%;
@@ -55,16 +56,12 @@ class App extends Component {
 
   state = {
     listMovies: [],
-    searchValue: ''
+    searchValue: '',
+    filteredList: []
   }
 
   async componentDidMount() {
     const response = await apiMovies.get()
-    console.log(response.data.results)
-
-    this.setState({
-      listMovies: response.data.results
-    })
 
     const moviesImgs = response.data.results.map((item) => {
       return {
@@ -74,38 +71,42 @@ class App extends Component {
     });
 
     this.setState({
-      listMovies: moviesImgs
+      listMovies: moviesImgs,
+      filteredList: moviesImgs
     });
   }
 
   handleChange = (event) => {
+    
+    const { listMovies } = this.state;
+
+   const filteredItems = listMovies.filter(item => {
+      if (event.target.value === '') {
+        return item
+      } else if (
+        item.title.toLowerCase().includes(event.target.value.toLowerCase())) {
+        return item
+      }
+    })
     this.setState({
-      searchValue: event.target.value
+      filteredList: filteredItems
     })
   }
 
   render() {
 
-    const { listMovies, searchValue } = this.state;
+    const { filteredList } = this.state;
 
     return (
       <Container>
         <BoxTitleSearch>
           <Title>Find movies</Title>
-          <InputSearch type="text" placeholder="E.g. Harry Potter" value={this.state.searchValue} onChange={this.handleChange} />
+          <InputSearch type="text" placeholder="E.g. Harry Potter" onChange={this.handleChange} />
         </BoxTitleSearch>
         <ContainerMain>
           <Main>
             <BoxMap>
-              {listMovies.filter(item => {
-                if (searchValue === '') {
-                  return item
-                } else if (
-                  item.title.toLowerCase().includes(searchValue.toLowerCase())) {
-                  return item
-                }
-              })
-                .slice(0, 8).map((item, index) => (
+              {filteredList.slice(0,8).map((item, index) => (
                   <div key={index}>
                     <ImgsMovie src={item.poster_path} alt="poster movies" />
                   </div>
